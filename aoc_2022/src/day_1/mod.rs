7e400c15a7422365b60dcfa482ddc951034bc7df
Kevin Moonen <dragonrider7225@gmail.com>
2022-12-01 00:39:25 -0600

    Solve AoC 2022 Day 1

--- aoc_2022/src/day_1/mod.rs
+++ aoc_2022/src/day_1/mod.rs
@@ -5,11 +5,48 @@ use std::{
 };
 
 fn part1(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Year 2022 Day 1 Part 1")
+    let mut snack_elf_calories = 0;
+    let mut current_elf_calories = 0;
+    let mut insert_calories = |current_elf_calories: &mut u32| {
+        snack_elf_calories = snack_elf_calories.max(mem::take(current_elf_calories));
+    };
+    for line in input.lines() {
+        let line = line?;
+        if line.trim().is_empty() {
+            insert_calories(&mut current_elf_calories);
+        } else {
+            current_elf_calories += line
+                .parse::<u32>()
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        }
+    }
+    insert_calories(&mut current_elf_calories);
+    Ok(snack_elf_calories)
 }
 
 fn part2(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Year 2022 Day 1 Part 2")
+    let mut snack_elf_calories = [0; 3];
+    let mut current_elf_calories = 0;
+    let mut insert_calories = |current_elf_calories: &mut u32| {
+        for place in snack_elf_calories.iter_mut() {
+            if *place < *current_elf_calories {
+                mem::swap(place, current_elf_calories);
+            }
+        }
+        *current_elf_calories = 0;
+    };
+    for line in input.lines() {
+        let line = line?;
+        if line.trim().is_empty() {
+            insert_calories(&mut current_elf_calories);
+        } else {
+            current_elf_calories += line
+                .parse::<u32>()
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        }
+    }
+    insert_calories(&mut current_elf_calories);
+    Ok(snack_elf_calories.into_iter().sum())
 }
 
 pub(super) fn run() -> io::Result<()> {
