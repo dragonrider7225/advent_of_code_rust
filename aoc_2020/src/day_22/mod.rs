4bbccd06b46ac7757d09f7d5e8e21092356d1974
Kevin Moonen <dragonrider7225@gmail.com>
2022-12-12 20:12:02 -0600

    Move AoC Year 2020 into a separate crate

---
+++ aoc_2020/src/day_22/mod.rs
@@ -0,0 +1,32 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+type Card = u32;
+
+fn part1(input: &mut dyn BufRead) -> io::Result<()> {
+    todo!("Year 2020 Day 22 Part 1")
+}
+
+fn part2(_input: &mut dyn BufRead) -> io::Result<()> {
+    todo!("Year 2020 Day 22 Part 2")
+}
+
+pub(super) fn run() -> io::Result<()> {
+    {
+        println!("Year 2020 Day 22 Part 1");
+        println!(
+            "{:?}",
+            part1(&mut BufReader::new(File::open("2020_22.txt")?))?
+        );
+    }
+    {
+        println!("Year 2020 Day 22 Part 2");
+        println!(
+            "{:?}",
+            part2(&mut BufReader::new(File::open("2020_22.txt")?))?
+        );
+    }
+    Ok(())
+}
---
+++ aoc_2020/src/day_23/mod.rs
@@ -0,0 +1,30 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+fn part1(_input: &mut dyn BufRead) -> io::Result<()> {
+    todo!("Year 2020 Day 23 Part 1")
+}
+
+fn part2(_input: &mut dyn BufRead) -> io::Result<()> {
+    todo!("Year 2020 Day 23 Part 2")
+}
+
+pub(super) fn run() -> io::Result<()> {
+    {
+        println!("Year 2020 Day 23 Part 1");
+        println!(
+            "{:?}",
+            part1(&mut BufReader::new(File::open("2020_23.txt")?))?
+        );
+    }
+    {
+        println!("Year 2020 Day 23 Part 2");
+        println!(
+            "{:?}",
+            part2(&mut BufReader::new(File::open("2020_23.txt")?))?
+        );
+    }
+    Ok(())
+}
