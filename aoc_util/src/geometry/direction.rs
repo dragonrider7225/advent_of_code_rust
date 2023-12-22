72cd38a067bbbe458b069af648b8a9d5bc2fd41d
Kevin Moonen <dragonrider7225@gmail.com>
2023-12-22 03:09:26 -0600

    Add 3D point and direction

--- aoc_util/src/geometry/direction.rs
+++ aoc_util/src/geometry/direction.rs
@@ -1,9 +1,9 @@
-use super::Point2D;
+use super::{Point2D, Point3D};
 use std::ops::{Add, AddAssign, Neg, Sub, SubAssign};
 
-/// A direction in 2-dimensional space.
+/// A cardinal direction in 2-dimensional space.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-pub enum Direction {
+pub enum Direction2D {
     #[allow(missing_docs)]
     Down,
     #[allow(missing_docs)]
@@ -14,47 +14,47 @@ pub enum Direction {
     Up,
 }
 
-impl Direction {
+impl Direction2D {
     /// All directions.
     pub const fn values() -> &'static [Self] {
         &[Self::Down, Self::Left, Self::Right, Self::Up]
     }
 }
 
-impl<T> Add<Direction> for Point2D<T>
+impl<T> Add<Direction2D> for Point2D<T>
 where
     T: Add<usize, Output = T>,
     T: Sub<usize, Output = T>,
 {
     type Output = Self;
 
-    fn add(self, rhs: Direction) -> Self::Output {
+    fn add(self, rhs: Direction2D) -> Self::Output {
         match rhs {
-            Direction::Down => self - Point2D::at(0, 1),
-            Direction::Left => self - Point2D::at(1, 0),
-            Direction::Right => self + Point2D::at(1, 0),
-            Direction::Up => self + Point2D::at(0, 1),
+            Direction2D::Down => self - Point2D::at(0, 1),
+            Direction2D::Left => self - Point2D::at(1, 0),
+            Direction2D::Right => self + Point2D::at(1, 0),
+            Direction2D::Up => self + Point2D::at(0, 1),
         }
     }
 }
 
-impl<T> AddAssign<Direction> for Point2D<T>
+impl<T> AddAssign<Direction2D> for Point2D<T>
 where
     T: AddAssign<usize>,
     T: SubAssign<usize>,
 {
-    fn add_assign(&mut self, rhs: Direction) {
+    fn add_assign(&mut self, rhs: Direction2D) {
         match rhs {
-            Direction::Down => *self -= Point2D::at(0, 1),
-            Direction::Left => *self -= Point2D::at(1, 0),
-            Direction::Right => *self += Point2D::at(1, 0),
-            Direction::Up => *self += Point2D::at(0, 1),
+            Direction2D::Down => *self -= Point2D::at(0, 1),
+            Direction2D::Left => *self -= Point2D::at(1, 0),
+            Direction2D::Right => *self += Point2D::at(1, 0),
+            Direction2D::Up => *self += Point2D::at(0, 1),
         }
     }
 }
 
-impl Neg for Direction {
-    type Output = Direction;
+impl Neg for Direction2D {
+    type Output = Direction2D;
 
     fn neg(self) -> Self::Output {
         match self {
@@ -62,6 +62,96 @@ impl Neg for Direction {
             Self::Left => Self::Right,
             Self::Right => Self::Left,
             Self::Up => Self::Down,
+        }
+    }
+}
+
+/// An unsigned axis in 3-space.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Axis3D {
+    #[allow(missing_docs)]
+    NorthSouth,
+    #[allow(missing_docs)]
+    EastWest,
+    #[allow(missing_docs)]
+    UpDown,
+}
+
+impl From<Direction3D> for Axis3D {
+    fn from(value: Direction3D) -> Self {
+        match value {
+            Direction3D::North | Direction3D::South => Self::NorthSouth,
+            Direction3D::East | Direction3D::West => Self::EastWest,
+            Direction3D::Up | Direction3D::Down => Self::UpDown,
+        }
+    }
+}
+
+/// A cardinal direction in 3-dimensional space.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Direction3D {
+    /// The positive x-axis.
+    East,
+    /// The negative x-axis.
+    West,
+    /// The positive y-axis.
+    North,
+    /// The negative y-axis.
+    South,
+    /// The positive z-axis.
+    Up,
+    /// The negative z-axis.
+    Down,
+}
+
+impl<T> Add<Direction3D> for Point3D<T>
+where
+    T: Add<usize, Output = T>,
+    T: Sub<usize, Output = T>,
+{
+    type Output = Self;
+
+    /// Take one step in the direction of the axis.
+    fn add(self, rhs: Direction3D) -> Self::Output {
+        match rhs {
+            Direction3D::East => self + Point3D::at(1, 0, 0),
+            Direction3D::West => self - Point3D::at(1, 0, 0),
+            Direction3D::North => self + Point3D::at(0, 1, 0),
+            Direction3D::South => self - Point3D::at(0, 1, 0),
+            Direction3D::Up => self + Point3D::at(0, 0, 1),
+            Direction3D::Down => self - Point3D::at(0, 0, 1),
+        }
+    }
+}
+
+impl<T> AddAssign<Direction3D> for Point3D<T>
+where
+    T: AddAssign<usize>,
+    T: SubAssign<usize>,
+{
+    fn add_assign(&mut self, rhs: Direction3D) {
+        match rhs {
+            Direction3D::East => *self += Point3D::at(1, 0, 0),
+            Direction3D::West => *self -= Point3D::at(1, 0, 0),
+            Direction3D::North => *self += Point3D::at(0, 1, 0),
+            Direction3D::South => *self -= Point3D::at(0, 1, 0),
+            Direction3D::Up => *self += Point3D::at(0, 0, 1),
+            Direction3D::Down => *self -= Point3D::at(0, 0, 1),
+        }
+    }
+}
+
+impl Neg for Direction3D {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Self::East => Self::West,
+            Self::West => Self::East,
+            Self::North => Self::South,
+            Self::South => Self::North,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
         }
     }
 }
--- aoc_util/src/geometry/mod.rs
+++ aoc_util/src/geometry/mod.rs
@@ -1,7 +1,7 @@
+/// Directions in 2- and 3-dimensional space.
+pub mod direction;
+pub use direction::{Axis3D, Direction2D as Direction, Direction3D};
+
 /// Locations in n-dimensional space.
 pub mod point;
-pub use point::Point2D;
-
-/// Directions in 2-dimensional space.
-pub mod direction;
-pub use direction::Direction;
+pub use point::{Point2D, Point3D};
