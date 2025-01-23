23ac62a14bb586690cc02995c3801d54635e8916
Kevin Moonen <dragonrider7225@gmail.com>
2025-01-22 22:24:20 -0600

    Implement a FILO linked list that shares its elements as much as possible

--- aoc_util/src/collections/mod.rs
+++ aoc_util/src/collections/mod.rs
@@ -1,3 +1,9 @@
 /// A priority queue has a constant-time lookup for the element with the greatest priority.
 pub mod priority_queue;
 pub use priority_queue::PriorityQueue;
+
+/// A FILO queue produces its elements in the reverse order that they were added. Two
+/// [`SharedFilo`]s created by pushing two different elements onto the same precursor will share
+/// memory for the parts that are identical.
+pub mod shared_filo;
+pub use shared_filo::SharedFilo;
