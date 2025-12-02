138d2d5283bf7c8e50a27c2e08e20eadc50d0879
Kevin Moonen <dragonrider7225@gmail.com>
2025-12-02 16:23:52 -0600

    Implement 2025 Day 1 Part 1

--- aoc_2025/src/day_1/mod.rs
+++ aoc_2025/src/day_1/mod.rs
@@ -3,8 +3,40 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-fn part1(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Year 2025 Day 1 Part 1")
+fn part1(input: &mut dyn BufRead) -> io::Result<usize> {
+    let instructions = input
+        .lines()
+        .map(|line| {
+            line.and_then(|line| match line.as_bytes()[0] {
+                b'L' => line[1..]
+                    .parse::<usize>()
+                    .map(|n| -(n as isize))
+                    .map_err(|e| {
+                        io::Error::new(
+                            io::ErrorKind::InvalidData,
+                            format!("{e:?}: {line:?} is not a valid instruction"),
+                        )
+                    }),
+                b'R' => line[1..].parse::<usize>().map(|n| n as isize).map_err(|e| {
+                    io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        format!("{e:?}: {line:?} is not a valid instruction"),
+                    )
+                }),
+                b => Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("'\\u{b:x}' does not start any valid instruction"),
+                )),
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    Ok(instructions
+        .into_iter()
+        .fold((0, 50), |(count, current), instr| {
+            let next = (current + instr) % 100;
+            (count + usize::from(next == 0), next)
+        })
+        .0)
 }
 
 fn part2(input: &mut dyn BufRead) -> io::Result<u32> {
@@ -27,4 +59,20 @@ pub(super) fn run() -> io::Result<()> {
         );
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::io::{self, Cursor};
+
+    #[test]
+    fn test_part1() -> io::Result<()> {
+        const TEST_DATA: &str = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n";
+        let expected = 3;
+        let actual = part1(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
 }
