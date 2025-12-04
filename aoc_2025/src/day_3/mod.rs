69e63df3598df87df16d6a95a0138984112c5bf9
Kevin Moonen <dragonrider7225@gmail.com>
2025-12-03 18:06:05 -0600

    Implement 2025 Day 3

--- aoc_2025/src/day_3/mod.rs
+++ aoc_2025/src/day_3/mod.rs
@@ -3,12 +3,55 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-fn part1(_input: &mut dyn BufRead) -> io::Result<()> {
-    todo!("Year 2025 Day 3 Part 1")
+fn bank_max(bank: &[usize], num_batteries: usize, prefix: usize) -> Option<usize> {
+    if bank.len() < num_batteries {
+        return None;
+    }
+    if num_batteries == 0 {
+        return Some(prefix);
+    }
+    (0..=9).rev().find_map(|digit| {
+        let pos = bank.iter().position(|&d| d == digit)?;
+        if pos + (num_batteries - 1) >= bank.len() {
+            return None;
+        }
+        bank_max(&bank[(pos + 1)..], num_batteries - 1, prefix * 10 + digit)
+    })
 }
 
-fn part2(_input: &mut dyn BufRead) -> io::Result<()> {
-    todo!("Year 2025 Day 3 Part 2")
+fn part1(input: &mut dyn BufRead) -> io::Result<usize> {
+    let banks = input
+        .lines()
+        .map(|line| {
+            line.map(|line| {
+                line.bytes()
+                    .map(|b| (b - b'0') as usize)
+                    .collect::<Vec<_>>()
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    eprintln!("{} banks of length {}", banks.len(), banks[0].len());
+    Ok(banks
+        .into_iter()
+        .map(|bank| bank_max(&bank[..], 2, 0).unwrap_or_else(|| panic!("Bank {bank:?} too small")))
+        .sum())
+}
+
+fn part2(input: &mut dyn BufRead) -> io::Result<usize> {
+    let banks = input
+        .lines()
+        .map(|line| {
+            line.map(|line| {
+                line.bytes()
+                    .map(|b| (b - b'0') as usize)
+                    .collect::<Vec<_>>()
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    Ok(banks
+        .into_iter()
+        .map(|bank| bank_max(&bank[..], 12, 0).unwrap_or_else(|| panic!("Bank {bank:?} too small")))
+        .sum())
 }
 
 pub(super) fn run() -> io::Result<()> {
@@ -27,4 +70,29 @@ pub(super) fn run() -> io::Result<()> {
         );
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use super::*;
+
+    const TEST_DATA: &str = "987654321111111\n811111111111119\n234234234234278\n818181911112111\n";
+
+    #[test]
+    fn test_part1() -> io::Result<()> {
+        let expected = 357;
+        let actual = part1(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> io::Result<()> {
+        let expected = 3_121_910_778_619;
+        let actual = part2(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
 }
